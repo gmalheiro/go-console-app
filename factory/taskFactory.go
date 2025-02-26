b9035d523164b35d2f21452758bdcbbc47348f23
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2025-02-25 22:55:19 -0300

    refactor: use of entity package

--- factory/taskFactory.go
+++ factory/taskFactory.go
@@ -1,9 +1,9 @@
 package factory
 
-import "go-console-app/model"
+import "go-console-app/entity"
 
-func CreateTask(title string, id int, status bool) model.Task {
-	return model.Task{
+func CreateTask(title string, id int, status bool) entity.Task {
+	return entity.Task{
 		ID:     id,
 		Title:  title,
 		Status: status,
